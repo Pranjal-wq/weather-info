87018fc16ffcd091c8e180c872d3efa13ec63491
pranjal jain <144061839+Pranjal-wq@users.noreply.github.com>
2024-06-26 12:35:09 +0530

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 let weather = {
-    "appKey": "ea43417a3b01f55d2cd07012a3f54d65",
+    appKey: "ea43417a3b01f55d2cd07012a3f54d65",
     fetchWeather: function (city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.appKey)
             .then((response) => response.json())
-            .then((data) => this.displayWeather(data));
+            .then((data) => this.displayWeather(data))
+            .catch((error) => console.error("Error fetching weather data:", error));
     },
     displayWeather: function (data) {
+        if (data.cod !== 200) {
+            console.error("Error: " + data.message);
+            return;
+        }
+        
         const { name } = data;
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        // console.log(name,icon,description,temp,humidity,speed);
+
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "http://openweathermap.org/img/w/" + icon + ".png";
         document.querySelector(".description").innerText = description;
@@ -18,21 +24,30 @@ let weather = {
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " kmph";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".weather").classList.remove("loading");
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+        
+        this.fetchBackgroundImage(name);
+    },
+    fetchBackgroundImage: function(city) {
+        const unsplashApiKey = "l6s_wOuHn_-nwcW0L6Wf70BY0gfaEFapkfUUzTenaHY"; // Replace with your Unsplash API key
+        const unsplashUrl = `https://api.unsplash.com/photos/random?query=${city}&client_id=${unsplashApiKey}`;
+        
+        fetch(unsplashUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                const imageUrl = data.urls.full;
+                document.body.style.backgroundImage = `url('${imageUrl}')`;
+            })
+            .catch((error) => console.error("Error fetching background image:", error));
     },
     search: function () {
         this.fetchWeather(document.querySelector(".search-bar").value);
     }
-}
+};
 
 let geocode = {
     reverseGeocode: function (latitude, longitude) {
         var api_key = '77630273a95846c8ad8583395eacd94b';
-        // var latitude = '51.0';
-        // var longitude = '7.0';
-
-        var api_url = 'https://api.opencagedata.com/geocode/v1/json'
-
+        var api_url = 'https://api.opencagedata.com/geocode/v1/json';
         var request_url = api_url
             + '?'
             + 'key=' + api_key
@@ -40,58 +55,44 @@ let geocode = {
             + '&pretty=1'
             + '&no_annotations=1';
 
-        // see full list of required and optional parameters:
-        // https://opencagedata.com/api#forward
-
         var request = new XMLHttpRequest();
         request.open('GET', request_url, true);
 
         request.onload = function () {
-            // see full list of possible response codes:
-            // https://opencagedata.com/api#codes
-
             if (request.status === 200) {
-                // Success!
                 var data = JSON.parse(request.responseText);
-                // console.log(data.results[0].components.city); // print the location
                 weather.fetchWeather(data.results[0].components.city);
             } else if (request.status <= 500) {
-                // We reached our target server, but it returned an error
-
-                console.log("unable to geocode! Response code: " + request.status);
+                console.log("Unable to geocode! Response code: " + request.status);
                 var data = JSON.parse(request.responseText);
-                console.log('error msg: ' + data.status.message);
+                console.log('Error msg: ' + data.status.message);
             } else {
-                console.log("server error");
+                console.log("Server error");
             }
         };
 
         request.onerror = function () {
-            // There was a connection error of some sort
-            console.log("unable to connect to server");
+            console.log("Unable to connect to server");
         };
 
-        request.send();  // make the request
-
+        request.send();
     },
 
     getLocation: function() {
         function success(data) {
-            geocode.reverseGeocode(data.coords.latitude,data.coords.longitude);
+            geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
         }
-        if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(success,console.error);
-        }
-        else {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, console.error);
+        } else {
             weather.fetchWeather("Bhopal");
         }
     }
-}
+};
 
-document.querySelector(".search button")
-    .addEventListener("click", function () {
-        weather.search()
-    });
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
 
 document.querySelector(".search-bar").addEventListener("keyup", function (event) {
     if (event.key == "Enter") {
@@ -100,4 +101,3 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
 });
 
 geocode.getLocation();
-// weather.fetchWeather("Bhopal"); default call for city in case we don't get geolocation navigator
